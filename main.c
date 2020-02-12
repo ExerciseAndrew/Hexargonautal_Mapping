0fb3ee0805972a3d98bd4a9dcd0e1229deea416a
ExerciseAndrew <53552952+ExerciseAndrew@users.noreply.github.com>
2020-02-12 14:46:14 -0800

    Add files via upload
    I am beginning to work on building a map editor for two dimensional hexagons

---
+++ main.c
@@ -0,0 +1,8 @@
+/// Map editing software project.
+/// Design a 2 dimensional (eventually) map/grid of interconnected and stacked hexagonal pieces
+/// Add map editing ability
+/// Add character editing
+/// Add new types of pieces with conditions to character status
+
+
+
